d79b67fb1bead309232750b3d43f737ad8ed48d8
Dennisbonke <dennisb@kabelfoon.net>
2014-12-08 18:37:41 +0100

    Episode 1

--- src/main/java/com/dennisbonke/letsmodng/LetsModNG.java
+++ src/main/java/com/dennisbonke/letsmodng/LetsModNG.java
@@ -1,10 +1,29 @@
 package com.dennisbonke.letsmodng;
 
 import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.Mod.EventHandler;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 
 @Mod(modid = LetsModNG.modid, version = LetsModNG.version)
 public class LetsModNG {
 
     public static final String modid = "letsmodng";
     public static final String version = "Alpha v0.1";
+
+    @EventHandler
+    public void preInit(FMLPreInitializationEvent event){
+
+    }
+
+    @EventHandler
+    public void Init(FMLInitializationEvent event){
+
+    }
+
+    @EventHandler
+    public void postInit(FMLPostInitializationEvent event){
+
+    }
 }
