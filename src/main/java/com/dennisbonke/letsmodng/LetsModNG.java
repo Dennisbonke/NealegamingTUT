5b66c60ba04e5f40d77b1bfc9800616f6a5f62b3
Dennisbonke <dennisb@kabelfoon.net>
2014-12-08 18:32:47 +0100

    Main mod class.

---
+++ src/main/java/com/dennisbonke/letsmodng/LetsModNG.java
@@ -0,0 +1,10 @@
+package com.dennisbonke.letsmodng;
+
+import cpw.mods.fml.common.Mod;
+
+@Mod(modid = LetsModNG.modid, version = LetsModNG.version)
+public class LetsModNG {
+
+    public static final String modid = "letsmodng";
+    public static final String version = "Alpha v0.1";
+}
