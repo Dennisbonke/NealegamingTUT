2d021222087759d70bd5346ca6141c6dd33d0ea0
Dennisbonke <dennisb@kabelfoon.net>
2014-12-09 19:10:46 +0100

    Episode 1.5

--- src/main/java/com/dennisbonke/letsmodng/LetsModNG.java
+++ src/main/java/com/dennisbonke/letsmodng/LetsModNG.java
@@ -1,10 +1,13 @@
 package com.dennisbonke.letsmodng;
 
+import com.dennisbonke.letsmodng.items.DBItems;
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.Mod.EventHandler;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+import cpw.mods.fml.common.registry.GameRegistry;
+import net.minecraft.item.Item;
 
 @Mod(modid = LetsModNG.modid, version = LetsModNG.version)
 public class LetsModNG {
@@ -12,8 +15,13 @@ public class LetsModNG {
     public static final String modid = "letsmodng";
     public static final String version = "Alpha v0.1";
 
+    public static Item itemCopperIngot;
+
     @EventHandler
     public void preInit(FMLPreInitializationEvent event){
+
+        itemCopperIngot = new DBItems().setUnlocalizedName("CopperIngot");
+        GameRegistry.registerItem(itemCopperIngot, "CopperIngot");
 
     }
 
---
+++ src/main/java/com/dennisbonke/letsmodng/items/DBItems.java
@@ -0,0 +1,19 @@
+package com.dennisbonke.letsmodng.items;
+
+import com.dennisbonke.letsmodng.LetsModNG;
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
+import net.minecraft.client.renderer.texture.IIconRegister;
+import net.minecraft.item.Item;
+
+public class DBItems extends Item{
+
+    public DBItems(){
+        this.setCreativeTab(getCreativeTab().tabMaterials);
+    }
+
+    @SideOnly(Side.CLIENT)
+    public void registerIcons(IIconRegister iconRegister){
+        this.itemIcon = iconRegister.registerIcon(LetsModNG.modid + ":" + this.getUnlocalizedName().substring(5));
+    }
+}
