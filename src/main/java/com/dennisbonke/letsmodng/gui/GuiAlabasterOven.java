84402e2945ad06ca98efcf92b7db2bb391f70e26
Dennisbonke <dennisb@kabelfoon.net>
2014-12-17 17:22:23 +0100

    Episode 13.4.

--- src/main/java/com/dennisbonke/letsmodng/gui/GuiAlabasterOven.java
+++ src/main/java/com/dennisbonke/letsmodng/gui/GuiAlabasterOven.java
@@ -1,10 +1,15 @@
 package com.dennisbonke.letsmodng.gui;
 
+import com.dennisbonke.letsmodng.LetsModNG;
 import com.dennisbonke.letsmodng.tileentity.TileEntityAlabasterOven;
 import net.minecraft.client.gui.inventory.GuiContainer;
+import net.minecraft.client.resources.I18n;
 import net.minecraft.entity.player.InventoryPlayer;
+import net.minecraft.util.ResourceLocation;
 
 public class GuiAlabasterOven extends GuiContainer{
+
+    public static final ResourceLocation bground = new ResourceLocation(LetsModNG.modid, "textures/gui/GuiAlabasterOven.png");
 
     public TileEntityAlabasterOven alabasterOven;
 
@@ -12,8 +17,18 @@ public class GuiAlabasterOven extends GuiContainer{
         super(ContainerAlabasterOven(inventoryPlayer, entity));
 
         this.alabasterOven = entity;
+
+        this.xSize = 176;
+        this.ySize = 166;
     }
 
+
+    public void drawGuiContainerForegroundLayer(int par1, int par2){
+        String name = this.alabasterOven.hasCustomInventoryName() ? this.alabasterOven.getInventoryName() : I18n.format(this.alabasterOven.getInventoryName(), new Object[0]);
+
+        this.fontRendererObj.drawString(name, this.xSize / 2 - this.fontRendererObj.getStringWidth(name) / 2, 6, 4210752);
+        this.fontRendererObj.drawString(I18n.format("Container.inventory", new Object[0]), 128, this.ySize - 96 + 2, 4210752);
+    }
 
     @Override
     protected void drawGuiContainerBackgroundLayer(float p_146976_1_, int p_146976_2_, int p_146976_3_) {
--- src/main/java/com/dennisbonke/letsmodng/tileentity/TileEntityAlabasterOven.java
+++ src/main/java/com/dennisbonke/letsmodng/tileentity/TileEntityAlabasterOven.java
@@ -1,12 +1,83 @@
 package com.dennisbonke.letsmodng.tileentity;
 
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.init.Items;
+import net.minecraft.inventory.ISidedInventory;
+import net.minecraft.item.ItemStack;
 import net.minecraft.tileentity.TileEntity;
 
-public class TileEntityAlabasterOven extends TileEntity{
+public class TileEntityAlabasterOven extends TileEntity implements ISidedInventory{
 
     private String localizedName;
 
+    private ItemStack[] slots = new ItemStack [3];
+
     public void setGuiDisplayName(String displayName) {
         this.localizedName = displayName;
+    }
+
+    public String getInventoryName(){
+        return this.hasCustomInventoryName() ? this.localizedName : "container.alabasterOven";
+    }
+
+    public boolean hasCustomInventoryName() {
+        return this.localizedName != null && this.localizedName.length() > 0;
+    }
+
+    @Override
+    public int getInventoryStackLimit() {
+        return 0;
+    }
+
+    @Override
+    public boolean isUseableByPlayer(EntityPlayer p_70300_1_) {
+        return false;
+    }
+
+    public void openInventory() {}
+    public void closeInventory() {}
+
+    @Override
+    public boolean isItemValidForSlot(int p_94041_1_, ItemStack p_94041_2_) {
+        return false;
+    }
+
+    public int getSizeInventory(){
+        return this.slots.length;
+    }
+
+    @Override
+    public ItemStack getStackInSlot(int p_70301_1_) {
+        return null;
+    }
+
+    @Override
+    public ItemStack decrStackSize(int p_70298_1_, int p_70298_2_) {
+        return null;
+    }
+
+    @Override
+    public ItemStack getStackInSlotOnClosing(int p_70304_1_) {
+        return null;
+    }
+
+    @Override
+    public void setInventorySlotContents(int p_70299_1_, ItemStack p_70299_2_) {
+
+    }
+
+    @Override
+    public int[] getAccessibleSlotsFromSide(int p_94128_1_) {
+        return new int[0];
+    }
+
+    @Override
+    public boolean canInsertItem(int i, ItemStack itemstack, int j) {
+        return this.isItemValidForSlot(i, itemstack);
+    }
+
+    @Override
+    public boolean canExtractItem(int i, ItemStack itemstack, int j) {
+        return j != 0 || i != 1 || itemstack.getItem() == Items.bucket;
     }
 }
